c3ef67323999ad73b34512c2c7d3094d2565813c
thiagobello <thiago_bello8@hotmail.com>
2018-03-19 11:59:08 -0300

    novo layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,89 +1,76 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Portal de Noticias Esamc</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link rel="stylesheet" type="text/css" href="assets/font/font-awesome.min.css" />
-	<link rel="stylesheet" type="text/css" href="assets/font/font.css" />
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" media="screen" />
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css" media="screen" />
-	<link rel="stylesheet" type="text/css" href="assets/css/responsive.css" media="screen" />
-	<link rel="stylesheet" type="text/css" href="assets/css/jquery.bxslider.css" media="screen" />
+<!doctype html>
+<html lang="pt">
+  <head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="/assets/css/material-kit.css">
+    <title>Portal de Noticias ESAMC</title>
+  </head>
+  <body>
 
-</head>
-<body>
-
-<div class="body_wrapper">
-  <div class="center">
-    <div class="header_area">
-      <div class="top_menu floatleft">
-      	<a href="#"><img src="assets/images/logo.png" alt=""  /></a>
-        <ul>
-          <li><a href="/noticias">Noticias</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact us</a></li>
-          <li><a href="#">Subscribe</a></li>
-          <li><a href="#">Login</a></li>
+    <div>
+        <ul class="nav nav-pills nav-pills-icons nav-pills-success" role="tablist">
+            <li class="nav-item">
+                <img src="/assets/img/esamc.png" align="center">
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#Login" role="tab" data-toggle="tab">
+                    <i class="material-icons">account_circle</i>
+                    Login
+                </a>
+            </li>            
         </ul>
-      </div>
-      <div class="social_plus_search floatright">
-        <div class="search">
-          <form action="#" method="post" id="search_form">
-            <input type="text" value="Search news" id="s" />
-            <input type="submit" id="searchform" value="search" />
-            <input type="hidden" value="post" name="post_type" />
-          </form>
-        </div>
-      </div>
-    </div>
-		
-	@yield('text')
 
-	<div class="footer_top_area">
-      <div class="inner_footer_top"> <img src="images/add3.png" alt="" /> </div>
     </div>
-    <div class="footer_bottom_area">
-      <div class="footer_menu">
-        <ul id="f_menu">
-          <li><a href="#">world news</a></li>
-          <li><a href="#">sports</a></li>
-          <li><a href="#">tech</a></li>
-          <li><a href="#">business</a></li>
-          <li><a href="#">Movies</a></li>
-          <li><a href="#">entertainment</a></li>
-          <li><a href="#">culture</a></li>
-          <li><a href="#">Books</a></li>
-          <li><a href="#">classifieds</a></li>
-          <li><a href="#">blogs</a></li>
-        </ul>
-      </div>
-      <div class="copyright_text">
-        <p> TCC </p>
-        <p> 2018 </p>
-      </div>
-    </div>
-  </div>
 
-<script type="text/javascript" src="assets/js/jquery-min.js"></script> 
-<script type="text/javascript" src="assets/js/bootstrap.min.js"></script> 
-<script type="text/javascript" src="assets/js/jquery.bxslider.js"></script> 
-<script type="text/javascript" src="assets/js/selectnav.min.js"></script> 
-<script type="text/javascript">
-selectnav('nav', {
-    label: '-Navigation-',
-    nested: true,
-    indent: '-'
-});
-selectnav('f_menu', {
-    label: '-Navigation-',
-    nested: true,
-    indent: '-'
-});
-$('.bxslider').bxSlider({
-    mode: 'fade',
-    captions: true
-});
-</script>
-</body>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-dark">
+            <div class="container">
+                <div class="navbar-translate">
+                    <a class="navbar-brand" href="/presentation.html">Home</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a href="#pablo" class="nav-link">link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#pablo" class="nav-link">link</a>
+                    </li>
+                </ul>
+
+                <form class="form-inline ml-auto">
+                    <div class="form-group has-white">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-white btn-raised btn-fab btn-fab-mini btn-round">
+                        <i class="material-icons">search</i>
+                    </button>
+                </form>
+            </div>
+            </div>
+        </nav>
+    </div>
+
+
+
+    @yield('text')
+
+    <script src="/assets/js/core/jquery.min.js"></script>
+    <script src="/assets/js/core/popper.min.js"></script>
+    <script src="/assets/js/bootstrap-material-design.js"></script>
+    <script src="/assets/js/plugins/bootstrap-datetimepicker.min.js"></script>
+    <script src="/assets/js/plugins/nouislider.min.js"></script>
+    <script src="/assets/js/material-kit.js?v=2.0.0"></script>
+
+  </body>
 </html>
